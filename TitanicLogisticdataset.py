2b93e653695fc5f893be45beb1a47ca5fb952375
Gauri <botregauri@gmail.com>
2020-01-16 20:57:31 +0530

    Initial commit

---
+++ TitanicLogisticdataset.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from seaborn import countplot
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure,show
+from sklearn.metrics import accuracy_score
